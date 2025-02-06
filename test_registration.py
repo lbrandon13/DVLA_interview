0b27ce08b349369998ed33059ec25b1a9b0845cc
lbrandon13 <31885900+lbrandon13@users.noreply.github.com>
2025-02-06 15:34:03 +0000

    Remove deprecated test

--- test_registration.py
+++ test_registration.py
@@ -4,15 +4,6 @@ from main import addNewRegistration, addIDToRegistration
 
 class TestRegistration(unittest.TestCase):
 
-    def testAddNewRegistration(self):
-
-        exampleDict = {"1234" : {}}
-        testDict = {}
-
-        addNewRegistration(testDict, "1234")
-
-        self.assertDictEqual(exampleDict, testDict)
-
     def testAddVehicleIDToRegistration(self):
 
         exampleDict = {"1234" : {"vehicleID" : "9876"}}
