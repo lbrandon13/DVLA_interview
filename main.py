942cbe70e24a879b1be9e7eafbb84828b6166e20
lbrandon13 <31885900+lbrandon13@users.noreply.github.com>
2025-02-06 11:12:22 +0000

    Add requirements comments

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+# Coding Exercise Part 1:
+#
+# The DVLA has a system for keeping track of vehicle registration numbers, any vehicle they are associated with and the year the registration was created.
+# A registration can be not assigned to a vehicle, but is still associated with a year.
+#
+# Create a suitable in memory object/structure to store such data and populate this with the data from the provided sheet.
+#
+# Feature 1:
+#
+# Return the total number of registration numbers.
 
+# Feature 2:
+
+# Return the vehicle ID for a given registration number.
