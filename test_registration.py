01d46379dcaec5f166dccbf252dc02cc659edde6
lbrandon13 <31885900+lbrandon13@users.noreply.github.com>
2025-02-12 15:50:13 +0000

    Delete deprecated and previously commented out code and tests

--- test_registration.py
+++ test_registration.py
@@ -1,51 +1,8 @@
 import unittest
 
-# from main import addNewRegistration, addIDToRegistration, countNumberOfRegistrations, getVehicleID
 from registry import Registry
 
 class TestRegistration(unittest.TestCase):
-
-    # def testAddVehicleIDToRegistration(self):
-
-    #     exampleDict = {"1234" : {"vehicleID" : "9876"}}
-    #     testDict = {"1234" : {}}
-
-    #     addIDToRegistration(testDict, "1234", "9876")
-
-    #     self.assertDictEqual(exampleDict, testDict)
-
-    # def testAddRegistrationWithYear(self):
-
-    #     exampleDict = {"1234" : {"year" : "2021"}}
-    #     testDict = {}
-
-    #     addNewRegistration(testDict, "1234", "2021")
-
-    #     self.assertDictEqual(exampleDict, testDict)
-
-    # def testCountRegistrations(self):
-
-    #     exampleDict = {'AB01 CDE': {'year': '2001', 'vehicleID': '12345'}, 'FG02 HJK': {'year': '2002', 'vehicleID': '67890'}, 'L33T H4X0R': {'year': '2022'}}
-
-    #     self.assertEqual(3, countNumberOfRegistrations(exampleDict))
-
-    # def testGetVehicleID(self):
-        
-    #     exampleDict = {"1234" : {'year': "2021", "vehicleID": "9876"}}
-
-    #     self.assertEqual("9876", getVehicleID(exampleDict, "1234"))
-
-    # def testAddToMissingRegistration(self):
-
-    #     exampleDict = {}
-
-    #     self.assertRaises(Exception, addIDToRegistration, exampleDict, "1234", "9876")
-
-    # def testGetMissingRegistration(self):
-
-    #     exampleDict = {}
-
-    #     self.assertRaises(Exception, getVehicleID, exampleDict, "1234")
 
     def testCreateRegistryObject(self):
 
