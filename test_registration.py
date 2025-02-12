fad8023d1a148baee2107bc3208683c396bd279d
lbrandon13 <31885900+lbrandon13@users.noreply.github.com>
2025-02-12 15:53:33 +0000

    Refactor setup of empty registry object to the setUp method

--- test_registration.py
+++ test_registration.py
@@ -4,6 +4,9 @@ from registry import Registry
 
 class TestRegistration(unittest.TestCase):
 
+    def setUp(self):
+        self.testRegistry = Registry()
+
     def testCreateRegistryObject(self):
 
         exampleRegistry = Registry()
@@ -12,49 +15,41 @@ class TestRegistration(unittest.TestCase):
         
         exampleDict = {"1234" : {"year" : "9876"}}
 
-        testRegistry = Registry()
-        testRegistry.addNewRegistration("1234", "9876")
+        self.testRegistry.addNewRegistration("1234", "9876")
 
-        self.assertEqual(exampleDict, testRegistry.registry)
+        self.assertEqual(exampleDict, self.testRegistry.registry)
 
     def testAddIDToRegistration(self):
 
         exampleDict = {"1234" : {"year" : "9876", "vehicleID" : "ABCD"}}
 
-        testRegistry = Registry()
-        testRegistry.addNewRegistration("1234", "9876")
-        testRegistry.addIDToRegistration("1234", "ABCD")
+        self.testRegistry.addNewRegistration("1234", "9876")
+        self.testRegistry.addIDToRegistration("1234", "ABCD")
 
-        self.assertEqual(exampleDict, testRegistry.registry)
+        self.assertEqual(exampleDict, self.testRegistry.registry)
 
     def testGetVehicleID(self):
 
-        testRegistry = Registry()
-        testRegistry.addNewRegistration("1234", "9876")
-        testRegistry.addIDToRegistration("1234", "ABCD")
+        self.testRegistry.addNewRegistration("1234", "9876")
+        self.testRegistry.addIDToRegistration("1234", "ABCD")
 
-        self.assertEqual(testRegistry.getVehicleID("1234"), "ABCD")
+        self.assertEqual(self.testRegistry.getVehicleID("1234"), "ABCD")
 
     def testCountRegistrations(self):
 
-        testRegistry = Registry()
-        testRegistry.addNewRegistration("12", "98")
-        testRegistry.addNewRegistration("123", "987")
-        testRegistry.addNewRegistration("1234", "9876")
+        self.testRegistry.addNewRegistration("12", "98")
+        self.testRegistry.addNewRegistration("123", "987")
+        self.testRegistry.addNewRegistration("1234", "9876")
 
-        self.assertEqual(testRegistry.countRegistrations(), 3)
+        self.assertEqual(self.testRegistry.countRegistrations(), 3)
 
     def testAddIDToMissingRegistration(self):
 
-        testRegistry = Registry()
-        
-        self.assertRaises(Exception, testRegistry.addIDToRegistration, "1234", "ABCD")
+        self.assertRaises(Exception, self.testRegistry.addIDToRegistration, "1234", "ABCD")
 
     def testGetIDFromMissingRegistration(self):
 
-        testRegistry = Registry()
-
-        self.assertRaises(Exception, testRegistry.countRegistrations, "1234")
+        self.assertRaises(Exception, self.testRegistry.countRegistrations, "1234")
 
 if __name__ == "__main__":
     unittest.main()
