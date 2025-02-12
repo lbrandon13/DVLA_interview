4b9c71e106a78fdf81beb2b1ff890dc791de9fe0
lbrandon13 <31885900+lbrandon13@users.noreply.github.com>
2025-02-12 14:13:52 +0000

    Add file to hold registry class code

---
+++ registry.py
@@ -0,0 +1,5 @@
+# shift registry into being an attribute of an registry object, and the various getting and updating functions as being methods of the registry class
+# 
+# initially implement a barebones registry class and then add each method, again through TDD
+# 
+# then deprecate each test for the old functions and then their appropriate code sections 
